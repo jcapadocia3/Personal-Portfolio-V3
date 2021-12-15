e358b1c8a379b243237470cdf57c8ff8b791fb72
James Capadocia <james.capadocia@gmail.com>
2021-12-15 13:16:23 -0600

    Setup file structure to determine when and how React pages will be rendered.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Work from './pages/Work';
 
 export default function Portfolio() {
-    console.log('Your site works!');
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Work') {
+      return <Work />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
+
+// import React from 'react';
+
+// export default function Portfolio() {
+//     console.log('Your site works!');
   
-    return (
-      <div>
-          <h1>Hello world!</h1>
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//           <h1>Hello world!</h1>
+//       </div>
+//     );
+//   }
