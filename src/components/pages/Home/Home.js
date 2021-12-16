df2084a3df49eaabdbc452e0c4806ec396025297
James Capadocia <james.capadocia@gmail.com>
2021-12-15 21:01:21 -0600

    Basic formatting stylings and cleanup of markup.

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './style.css';
+import "./Home.css";
 
 export default function Home() {
   return (
@@ -7,56 +7,6 @@ export default function Home() {
       <div class="leftCont">
         <div class="avatar"></div>
         <h1>James Capadocia</h1>
-        <div class="navLinks">
-          <p>
-            <a id="fastforward" href="index.html">
-              Home
-            </a>
-          </p>
-          <p>
-            <a id="fastforward" href="aboutme.html">
-              About Me
-            </a>
-          </p>
-          <p>
-            <a id="fastforward" href="work.html">
-              Work
-            </a>
-          </p>
-          <div class="dropdownMenu">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuBtn"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Contact Me
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <p class="dropdown-item">P: 847.730.7599</p>
-              <p class="dropdown-item">E: james.capadocia@gmail.com</p>
-              <a
-                class="dropdown-item2 dropdownLink blue"
-                href="https://linkedin.com/in/james-capadocia-156367217"
-              >
-                <span class="linkStyle">LinkedIn</span>
-              </a>
-              <a
-                class="dropdown-item2 dropdownLink blue"
-                href="https://github.com/jcapadocia3"
-              >
-                <span class="linkStyle">Github</span>
-              </a>
-            </div>
-          </div>
-          <p>
-            <a id="fastforward" href="./assets/images/NEW Resume.pdf">
-              Resume
-            </a>
-          </p>
-        </div>
       </div>
     </div>
   );
