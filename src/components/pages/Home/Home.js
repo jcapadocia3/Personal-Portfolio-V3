1ccd067d58022ec2c931978de14cfa8c43b791a1
James Capadocia <james.capadocia@gmail.com>
2021-12-16 18:37:18 -0600

    Got avatar image to show up on my home page.

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.css";
-import Avatar from "..../assets/IMG_8160c.jpg"
+import Avatar from "./assets/IMG_8160c.jpg"
 
 export default function Home() {
   return (
     <div>
-      <img src= {Avatar} thumbnail style={{maxHeight:"10rem"}}/>
+      <img class="avatar" src= {Avatar} />
     </div>
   );
 }
